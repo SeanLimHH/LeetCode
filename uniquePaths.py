3dda117376a306fbae9bd45438a501f07a2ed9f9
Sean <sean123lim@gmail.com>
2023-04-17 00:07:43 +0800

    Unique Paths

--- uniquePaths.py
+++ uniquePaths.py
@@ -1,5 +1,5 @@
 '''
-Problem: Medium
+Problem : Medium
 
 There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
 
@@ -11,12 +11,21 @@ https://leetcode.com/problems/unique-paths/description/
 
 Personal Interpretations:
 
-Input: Two integers representing the row and column size of the grid.
-Output: Total unique paths to get from top left of grid to bottom right of grid.
+Input : Two integers representing the row and column size of the grid
 
-Subproblem: Given current grid, what is the number of ways to get here?
+Output : Total unique paths to get from top left of grid to bottom right of grid
 
-By solving this, we can chain-construct the number of ways to get to any grid we want.
+Subproblem : Given current grid, what is the number of ways to get here?
+
+Optimal-substructure? : Yes, because solving a subproblem leads to a solution to the main problem
+
+State variable(s) : The number of unique ways to reach current grid
+
+Decision variable(s) : Travel either right or down
+
+Recurrence Formula : F(row, col) = F(row - 1, col) + F(row, col - 1)
+
+By solving this, we can chain-construct the number of ways to get to any grid we want
 
 It can also be solved via math through permutations and combinations.
 '''
