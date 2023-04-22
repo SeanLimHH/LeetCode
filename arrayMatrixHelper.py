4854da8695be67046f8b026664e6089e1fb87579
SeanLimHH <53903834+SeanLimHH@users.noreply.github.com>
2023-04-22 22:32:33 +0800

    Added functionality and made it more informative
    Modified printing of matrices and or lists.
    
    Added function to creating random 2D arrays of data type string, float or integers with explicit values and sizes and dimensions.
    
    Added a basic function to create a list with required size and initial value.
    
    Added error handling for all functions.

--- arrayMatrixHelper.py
+++ arrayMatrixHelper.py
@@ -1,26 +1,88 @@
+def createList(size, initialisedValue = 0):
+    return [initialisedValue for _ in range(size)]
+
+def printList(list_):
+    try:
+        if (isinstance(list_[0],list)):
+            print("Error occurred.")
+            print("The input for printList() is not a list! Please input the correct data structure!")
+            return None
+        
+        if (isinstance(list_,str)):
+            print("Error occurred.")
+            print("The input for printList() is not a list! Please input the correct data structure!")
+            return None
+        
+    except TypeError:
+            print("Error occurred.")
+            print("The input for printList() is not a list! Please input the correct data structure!")
+            return None
+    
+    print("Display of list:")
+    print("Number of columns (size):",len(list_))
+    print()
+    maxLengthForFormatting = 0
+
+    for element in list_:
+        if len(str(element)) > maxLengthForFormatting:
+                maxLengthForFormatting = len(str(element))
+
+    for element in list_:
+        print(f"{element:<{2*maxLengthForFormatting}}  ", end="")
+    print("\n")
 
 def create2DMatrix(rows, columns, initialisedValue = 0):
-    return [[initialisedValue for i in range(columns)] for j in range(rows)]
+    return [[initialisedValue for _ in range(columns)] for _ in range(rows)]
 
-def printMatrix(matrix):
+def print2DMatrix(matrix):
+    try:
+        if (not isinstance(matrix[0],list)):
+            print("Error occurred.")
+            print("The input for print2DMatrix() is not a 2D array or matrix! Please input the correct data structure!")
+            return None
+        
+        if (isinstance(matrix, str)):
+            print("Error occurred.")
+            print("The input for print2DMatrix() is not a list! Please input the correct data structure!")
+            return None
+        
+    except TypeError:
+        print("Error occurred.")
+        print("The input for print2DMatrix() is not a 2D array or matrix! Please input the correct data structure!")
+        return None
+    
     print("Display of two-dimensional matrix:")
     print("Number of rows:",len(matrix))
     print("Number of columns:",len(matrix[0]))
+    print()
+
+    maxLengthForFormatting = 0
 
     for row in matrix:
         for col in row:
-            print(f"{col}  ", end="")
+            if len(str(col)) > maxLengthForFormatting:
+                maxLengthForFormatting = len(str(col))
+                
+    for row in matrix:
+        for col in row:
+            print(f"{col:<{2*maxLengthForFormatting}}", end="")
         print()
+    print()
 
 def getEntry(matrix, rowIndex, columnIndex):
-    print("\ngetEntry():")
+    print("\ngetEntry(): Returns the entry in the matrix at row rowIndex and column columnIndex")
     try:
         rowIndex = int(rowIndex)
         columnIndex = int(columnIndex)
+
         if rowIndex >= len(matrix):
+            print("Error occurred.")
             print("Row index is larger than number of rows! Please enter a valid row index!")
+
         if columnIndex >= len(matrix):
+            print("Error occurred.")
             print("Column index is larger than number of columns! Please enter a valid column index!")
+
     except ValueError:
         print("Please enter a valid integer >= 0 for row index and column index!")
         return None
@@ -29,26 +91,33 @@ def getEntry(matrix, rowIndex, columnIndex):
     return matrix[rowIndex][columnIndex]
 
 def updateEntry(matrix, rowIndex, columnIndex, newValue):
-    print("\nupdateEntry():")
+    print("\nupdateEntry(): Updates entry of matrix. It is simply matrix[rowIndex][columnIndex] = value")
     try:
         rowIndex = int(rowIndex)
         columnIndex = int(columnIndex)
+
         if rowIndex >= len(matrix):
+            print("Error occurred.")
             print("Row index is larger than number of rows! Please enter a valid row index!")
+
         if columnIndex >= len(matrix):
+            print("Error occurred.")
             print("Column index is larger than number of columns! Please enter a valid column index!")
+            
     except ValueError:
         print("Please enter a valid integer >= 0 for row index and column index!")
         return None
     
     print(f"Value of cell to be updated: row index {rowIndex} column index {columnIndex}")
     print(f"Current value is: {matrix[rowIndex][columnIndex]}")
-    print(f"New value to be update in cell: {newValue}")
-    return matrix[rowIndex][columnIndex]
+    print(f"New value to be updated in cell [{rowIndex}][{columnIndex}]: {newValue}")
+    matrix[rowIndex][columnIndex] = newValue
+    return matrix
 
 def setLeftColumn(matrix, value):
-    print("\nsetLeftColumn():")
+    print("\nsetLeftColumn(): Sets all elements in the first column to the same value")
     if not isinstance(value, float) and not isinstance(value, int):
+        print("Error occurred.")
         print("Input value is not numeric! Please enter a valid value!")
         return None
 
@@ -57,11 +126,12 @@ def setLeftColumn(matrix, value):
     
     print(f"Setting left column to value: {value}")
     print("New matrix:")
-    printMatrix(matrix)
+    print2DMatrix(matrix)
 
 def setTopRow(matrix, value):
-    print("\nsetTopRow():")
+    print("\nsetTopRow(): Sets all elements in the first row to the same value")
     if not isinstance(value, float) and not isinstance(value, int):
+        print("Error occurred.")
         print("Input value is not numeric! Please enter a valid value!")
         return None
 
@@ -70,15 +140,17 @@ def setTopRow(matrix, value):
     
     print(f"Setting top row to value: {value}")
     print("New matrix:")
-    printMatrix(matrix)
+    print2DMatrix(matrix)
 
 def setDiagonal(matrix, value): # Assumes matrix is square!
-    print("\nsetDiagonal():")
+    print("\nsetDiagonal(): Sets all elements along the main diagonal to the same value. Assumes matrix is a square")
     if len(matrix) != len(matrix[0]):
+        print("Error occurred.")
         print("Matrix is not square! setDiagonal(matrix, value) will not work! Exiting...")
         return None
 
     if not isinstance(value, float) and not isinstance(value, int):
+        print("Error occurred.")
         print("Input value is not numeric! Please enter a valid value!")
         return None
 
@@ -87,4 +159,222 @@ def setDiagonal(matrix, value): # Assumes matrix is square!
     
     print(f"Setting diagonal to value: {value}")
     print("New matrix:")
-    printMatrix(matrix)
+    print2DMatrix(matrix)
+
+def rowsIn(matrix):
+    print("\nrowsIn(): Counts the number of rows in list or matrix")
+    if isinstance(matrix[0],list):
+        print(f"Matrix has {len(matrix[0])} rows.\n")
+        return (len(matrix[0]))
+    
+    elif isinstance(matrix, list):
+        print(f"List has 1 row.\n")
+        return 1
+    
+    else:
+        print("Error occurred.")
+        print("Data type is not a matrix or list!\n")
+        return None
+
+def columnsIn(matrix):
+    print("\ncolumnsIn(): Counts the number of columns in list or matrix")
+    if isinstance(matrix[0],list):
+        print(f"Matrix has {len(matrix[0])} columns.\n")
+        return (len(matrix[0]))
+        
+    elif isinstance(matrix, list):
+        print(f"List has {len(matrix)} columns.\n")
+        return (len(matrix))
+    
+    else:
+        print("Error occurred.")
+        print("Data type is not a matrix or list!\n")
+        return None
+
+def createListRandom(dataType, size, startingNumberInclusive = 0, endingNumberInclusive = 0, stringLength = 0, seed = 0):
+    
+    print("\ncreateListRandom(): Creates a random list. Some of the parameters are required; some are not. It will return error(s) if required parameter(s) are absent.")
+
+    try:
+        size = int(size)
+        if size <= 0:
+            print("Error occurred.")
+            print("Please enter a positive integer for your size parameter!")
+            return None
+
+    except ValueError:
+        print("Error occurred.")
+        print("Please enter an integer for your size parameter! It should be > 0 as well.")
+        return None
+    
+    import random
+    random.seed(seed)
+
+    match dataType:
+
+        case "str":
+
+            import string
+
+            lettersDigits = string.ascii_letters + string.digits
+
+            if stringLength == 0:
+                print("Error occurred.")
+                print("Please enter a valid string length!")
+                return None
+            
+            return [''.join(random.choice(lettersDigits) for _ in range(stringLength)) for _ in range(size)]
+            
+
+        case "int":
+            try:
+                startingNumberInclusive = int(startingNumberInclusive)
+
+            except ValueError:
+                print("Error occurred.")
+                print("Please enter an integer for your starting number. Ensure that the ending number (inclusive) should be >= starting number (inclusive)!")
+                return None
+            
+            try:
+                endingNumberInclusive = int(endingNumberInclusive)
+
+            except ValueError:
+                print("Error occurred.")
+                print("Please enter an integer for your ending number. Ensure that the ending number (inclusive) should be >= starting number (inclusive)!")
+                return None
+    
+
+            if startingNumberInclusive > endingNumberInclusive:
+                print("Error occurred.")
+                print("Please ensure that the ending number (inclusive) should be >= starting number (inclusive)!")
+                return None
+            
+            return [random.randrange(startingNumberInclusive, endingNumberInclusive) for _ in range(size)]
+        
+        case "float":
+            try:
+                startingNumberInclusive = int(startingNumberInclusive)
+
+            except ValueError:
+                print("Error occurred.")
+                print("Please enter an integer for your starting number. Ensure that the ending number (inclusive) should be >= starting number (inclusive)!")
+                return None
+            
+            try:
+                endingNumberInclusive = int(endingNumberInclusive)
+
+            except ValueError:
+                print("Error occurred.")
+                print("Please enter an integer for your ending number. Ensure that the ending number (inclusive) should be >= starting number (inclusive)!")
+                return None
+    
+
+            if startingNumberInclusive > endingNumberInclusive:
+                print("Error occurred.")
+                print("Please ensure that the ending number (inclusive) should be >= starting number (inclusive)!")
+                return None
+            
+            return [random.uniform(startingNumberInclusive, endingNumberInclusive) for _ in range(size)]
+        
+        case _:
+            print("Please enter a valid parameter type for dataType for createListRandom()!. It should either be 'int', 'float' or 'str'!")
+            return None
+
+def create2DMatrixRandom(dataType, rows = 1, columns = 1,startingNumberInclusive = 0, endingNumberInclusive = 10, stringLength = 0, seed = 0):
+
+    print("\ncreate2DMatrixRandom(): Creates a random matrix. Some of the parameters are required; some are not. It will return error(s) if required parameter(s) are absent.")
+
+    try:
+        rows = int(rows)
+        if rows <= 0:
+            print("Error occurred.")
+            print("Please enter a positive number for the number of rows!")
+            return None
+
+    except ValueError:
+        print("Error occurred.")
+        print("Please enter an integer for the number of rows. Ensure that the ending number (inclusive) should be >= starting number (inclusive)!")
+        return None
+
+    try:
+        columns = int(columns)
+        if columns <= 0:
+            print("Error occurred.")
+            print("Please enter a positive number for the number of columns!")
+            return None
+
+    except ValueError:
+        print("Error occurred.")
+        print("Please enter a positive integer for the number of columns!")
+        return None
+
+    import random
+    random.seed(seed)
+
+    if dataType == "str":
+
+        import string
+
+        if stringLength == 0:
+                print("Error occurred.")
+                print("Please enter a valid string length!")
+                return None
+        lettersDigits = string.ascii_letters + string.digits
+
+        return [[''.join(random.choice(lettersDigits) for _ in range(stringLength)) for _ in range(columns)] for _ in range(rows)]
+            
+
+    if dataType == "int":
+
+        try:
+            startingNumberInclusive = int(startingNumberInclusive)
+
+        except ValueError:
+            print("Error occurred.")
+            print("Please enter an integer for your starting number. Ensure that the ending number (inclusive) should be >= starting number (inclusive)!")
+            return None
+        
+        try:
+            endingNumberInclusive = int(endingNumberInclusive)
+
+        except ValueError:
+            print("Error occurred.")
+            print("Please enter an integer for your ending number. Ensure that the ending number (inclusive) should be >= starting number (inclusive)!")
+            return None
+
+
+        if startingNumberInclusive > endingNumberInclusive:
+            print("Error occurred.")
+            print("Please ensure that the ending number (inclusive) should be >= starting number (inclusive)!")
+            return None
+            
+ 
+        return [[random.randrange(startingNumberInclusive, endingNumberInclusive) for _ in range(columns)] for _ in range(rows)]
+        
+    
+
+    if dataType == "float":
+        
+        try:
+            startingNumberInclusive = int(startingNumberInclusive)
+
+        except ValueError:
+            print("Error occurred.")
+            print("Please enter an integer for your starting number. Ensure that the ending number (inclusive) should be >= starting number (inclusive)!")
+            return None
+        
+        try:
+            endingNumberInclusive = int(endingNumberInclusive)
+
+        except ValueError:
+            print("Error occurred.")
+            print("Please enter an integer for your ending number. Ensure that the ending number (inclusive) should be >= starting number (inclusive)!")
+            return None
+
+
+        if startingNumberInclusive > endingNumberInclusive:
+            print("Error occurred.")
+            print("Please ensure that the ending number (inclusive) should be >= starting number (inclusive)!")
+            return None
+            
+        return [[random.uniform(startingNumberInclusive, endingNumberInclusive) for _ in range(columns)] for _ in range(rows)]
